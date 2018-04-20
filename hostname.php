683a4a2040b699c0177012d7eace50f1494515ab
prasadambasaveswar <basaveswar01@gmail.com>
2018-04-20 14:36:30 +0530

    update

--- hostname.php
+++ hostname.php
@@ -1,6 +1,3 @@
 <?php
-echo gethostname(); // may output e.g,: sandie
-
-// Or, an option that also works before PHP 5.3
-echo php_uname('n'); // may output e.g,: sandie
+echo "hostname name is " . gethostname();
 ?>
